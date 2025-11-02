113cef5b45d710b23fa0e3231c262cb0f0f48667
Rafaell <rafaellgramata01@gmail.com>
2025-11-02 03:51:00 -0800

    created trainer: plan

--- app/src/main/java/com/example/vimora/SignupActivity.java
+++ app/src/main/java/com/example/vimora/SignupActivity.java
@@ -1,6 +1,9 @@
 package com.example.vimora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
@@ -8,5 +11,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Redirect to SignupActivity when image is clicked
+        ImageView signupButton = findViewById(R.id.signupSubmit);
+        signupButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this, TrainerPlanActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/vimora/SplashActivity.java
+++ app/src/main/java/com/example/vimora/SplashActivity.java
@@ -15,13 +15,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Navigate to WelcomeActivity after 3 seconds
+        // Navigate to WelcomeActivity after 1 second
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
                 finish();
             }
-        }, 3000);
+        }, 1000);
     }
 }
