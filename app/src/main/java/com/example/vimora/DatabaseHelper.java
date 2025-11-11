405759c929598334ed0761a3581260ef7ba648d0
Null <nullv@student.douglascollege.ca>
2025-11-10 17:34:44 -0800

    Progress on making signup system.
    Doesn't do anything at the moment but it's closer than before and I'm
    going home now.

--- app/src/main/java/com/example/vimora/DatabaseHelper.java
+++ app/src/main/java/com/example/vimora/DatabaseHelper.java
@@ -1,16 +1,29 @@
 package com.example.vimora;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.Normalizer;
+import java.util.Calendar;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
+    MessageDigest md;
     final static String DATABASE_NAME = "Vimora";
-    final static int DATABASE_VERSION = 3;
+    final static int DATABASE_VERSION = 3; // why did I make this "3" for the first version?
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e ) {
+            // impossible
+        }
     }
     @Override
     public void onConfigure(SQLiteDatabase sqLiteDatabase) {
@@ -19,12 +32,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE User (userID INTEGER PRIMARY KEY, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, phone TEXT, passwordHash TEXT NOT NULL, isTrainer INTEGER NOT NULL, trainerID INTEGER, trainieeHeight REAL, trainieeBirthday INTEGER, trainerAbout TEXT, FOREIGN KEY (trainerID) REFERENCES User(userID))");
+        db.execSQL("CREATE TABLE User (" +
+                "userID INTEGER PRIMARY KEY, " +
+                "name TEXT NOT NULL, " +
+                "email TEXT UNIQUE NOT NULL, " +
+                "phone TEXT, " +
+                "passwordHash TEXT NOT NULL, " +
+                "isTrainer INTEGER NOT NULL, " +
+                "trainerID INTEGER, " +
+                "trainieeHeight REAL, " +
+                "trainieeBirthday INTEGER, " +
+                "trainerAbout TEXT, " +
+                "FOREIGN KEY (trainerID) REFERENCES User(userID))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS User");
         onCreate(db);
+    }
+
+    public boolean signUp(String name, String phone, String email, String password, boolean isTrainer, float height, Calendar birthday) {
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name",name);
+        values.put("phone",phone);
+        values.put("email",email);
+        values.put("passwordHash",hashPassword(password));
+        values.put("isTrainer",isTrainer);
+        if (!isTrainer) {
+            values.put("traineeHeight",height);
+            values.put("traineeBirthday",birthday.get(Calendar.YEAR)*1000+birthday.get(Calendar.DAY_OF_YEAR));
+        }
+        long r = sqLiteDatabase.insert("User",null,values);
+        return r>0;
+    }
+
+    private String hashPassword(String password) { // we don't want to store the password raw in the database
+        String normalized = java.text.Normalizer.normalize(password, Normalizer.Form.NFKD);
+        byte[] digest = md.digest(normalized.getBytes(StandardCharsets.UTF_8));
+        return android.util.Base64.encodeToString(digest,0);
     }
 }
--- app/src/main/java/com/example/vimora/SignupActivity.java
+++ app/src/main/java/com/example/vimora/SignupActivity.java
@@ -2,20 +2,38 @@ package com.example.vimora;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.vimora.trainer.TrainerProfileActivity;
 
 public class SignupActivity extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        databaseHelper = new DatabaseHelper(this);
 
+        TextView name = findViewById(R.id.signupName);
+        TextView phone = findViewById(R.id.signupPhone);
+        TextView email = findViewById(R.id.signupEmail);
+        TextView password = findViewById(R.id.signupPassword);
+        Spinner trainerTrainee = findViewById(R.id.signupTrainerTrainee);
         ImageView signupButton = findViewById(R.id.signupSubmit);
         signupButton.setOnClickListener(v -> {
-            Intent intent = new Intent(SignupActivity.this, TrainerProfileActivity.class);
-            startActivity(intent);
+            boolean signedUp = databaseHelper.signUp(name.getText().toString(),phone.getText().toString(),email.getText().toString(),password.getText().toString(),trainerTrainee.getSelectedItemPosition()==1,0.0f,null);
+            // TODO for now, height and birthday remain unset. also doing nothing with weight.
+            if (signedUp) {
+                Toast.makeText(SignupActivity.this,"Successfully signed up",Toast.LENGTH_LONG); // TODO doesn't actually appear
+                Intent intent = new Intent(SignupActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+            else Toast.makeText(SignupActivity.this,"Could not sign up",Toast.LENGTH_LONG);
         });
     }
 }
