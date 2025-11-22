81401fadc39fc37bd8d54bf5f9967fd737f8627d
300401988 <nullv@student.douglascollege.ca>
2025-11-22 12:38:21 -0800

    working on reminders. saving progress while i move to another computer

--- app/src/main/java/com/example/vimora/trainer/TrainerRemindActivity.java
+++ app/src/main/java/com/example/vimora/trainer/TrainerRemindActivity.java
@@ -1,8 +1,7 @@
-package com.example.vimora.trainee;
+package com.example.vimora.trainer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,25 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.vimora.DatabaseHelper;
 import com.example.vimora.R;
 
-public class TraineeRemindActivity extends AppCompatActivity {
+public class TrainerRemindActivity extends AppCompatActivity {
     DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_trainee_remind01);
+        databaseHelper = new DatabaseHelper(this);
+        setContentView(R.layout.activity_trainer_profile3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvName), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        databaseHelper = new DatabaseHelper(this);
         Intent intent = getIntent();
-        long userID = intent.getLongExtra("userID",-1);
-
-        TextView trainerName = findViewById(R.id.txtCoach);
-
-        trainerName.setText(databaseHelper.getName(databaseHelper.getTraineeTrainer(userID)));
+        long userID = intent.getLongExtra("userID", -1);
     }
 }
